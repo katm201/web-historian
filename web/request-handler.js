1d5e9a72f1ad3fed86f46e50bb54365b966118ef
katm201 <katm201@gmail.com>
2017-11-01 17:26:57 -0700

    Returning contents of index.html.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,19 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers.js');
+var fs = require('fs');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+exports.handleRequest = function (request, response) {
+  if (request.method === 'GET') {
+    fs.readFile('web/public/index.html', 'utf8', function(err, data) {
+      if (err) {
+        console.error(err);
+      }
+      response.writeHead(200, httpHelpers.headers);
+      response.end(data);
+    });
+    
+  }
+  // response.end(archive.paths.list);
 };
