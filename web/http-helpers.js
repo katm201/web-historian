f36744dd6d99e600dc075d22b16e409c37f0d12d
katm201 <katm201@gmail.com>
2017-11-02 12:47:05 -0700

    Added a check for our css files when loading our html pages.

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -15,12 +15,20 @@ exports.serveAssets = function(response, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  var needCSS = asset.slice(asset.length - 4) === 'html';
   
   fs.readFile(asset, 'utf8', function(err, data) {
     if (err) {
       console.error(err);
     }
-    response.end(data);
+    // adds our css file for our index or loading page
+    if (needCSS) {
+      fs.readFile('web/public/styles.css', 'utf8', function(err, cssData) {
+        response.end('<style>' + cssData + '</style>' + data);
+      }); 
+    } else {
+      response.end(data);
+    }
   });
 };
 
